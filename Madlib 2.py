d65447cef47703168841f9ae371fdd933cb250e5
NekoBrewer <78179642+NekoBrewer@users.noreply.github.com>
2021-02-02 14:26:31 +1300

    Madlib
    Aks you questions then makes a story and they're finished

---
+++ Madlib 2.py
@@ -0,0 +1,7 @@
+name = input("welcome to my Madlib enter a name  ")
+body_part = input("enter a body part  ")
+illness = input("enter an illness  ")
+school_subject = input("enter a school subject  ")
+number = input("enter a number greater than 1  ")
+
+print("hello, {} your {} looks extremely painful and I think you should excuse yourself from {} because you might have {}".format(name,body_part,school_subject,illness)
--- Madlib Program.py
+++ Madlib Program.py
@@ -1,10 +1,10 @@
-#this code makes a story by just asking a few questions
 
-#
+
+# this part asks the person some basic questions which they can answer
 name = input("welcome to my Madlib enter a name  ")
 body_part = input("enter a body part  ")
 illness = input("enter an illness  ")
 school_subject = input("enter a school subject  ")
 number = input("enter a number greater than 1  ")
-
+#this creates a story from the answers that were entered
 print("To whom it may concern please excuse", name, "from", school_subject, "he is suffering from a horrible case of", illness, "which is causing great pain to his", body_part, "he will therefore be away from school for the next", number, "days")
