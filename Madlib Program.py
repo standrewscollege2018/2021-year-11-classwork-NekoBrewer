3c49a717693d2ceaa163132cf17271a2c94fdd2d
NekoBrewer <78179642+NekoBrewer@users.noreply.github.com>
2021-02-02 14:03:33 +1300

    Madlib program
    Asks someone how multiple question and then creates a story with their answers.

---
+++ Madlib Program.py
@@ -0,0 +1,10 @@
+#this code makes a story by just asking a few questions
+
+#
+name = input("welcome to my Madlib enter a name  ")
+body_part = input("enter a body part  ")
+illness = input("enter an illness  ")
+school_subject = input("enter a school subject  ")
+number = input("enter a number greater than 1  ")
+
+print("To whom it may concern please excuse", name, "from", school_subject, "he is suffering from a horrible case of", illness, "which is causing great pain to his", body_part, "he will therefore be away from school for the next", number, "days")
