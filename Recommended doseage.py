c119e8e0b5982878d6e70897bba6d35e44e2355b
NekoBrewer <78179642+NekoBrewer@users.noreply.github.com>
2021-02-05 10:41:31 +1300

    Recommended Doseage
    Asks for your age and if you're 12 or older you'll be recommended 2 500mg tables but if you're under 12 you'll be asked your weight and then your weight is multiplied by 10mg and that is your recommended doseage.

--- Recommended doseage.py
+++ Recommended doseage.py
@@ -1,11 +1,14 @@
 REQUIRED_AGE = 12
 DOSEAGE = 10
-AGE = int(input("How old are you "))
+age = int(input("How old are you "))
 
-
-if AGE >= REQUIRED_AGE:
-   print("Recommended two 500mg tablets of paracetamol")
-elif AGE < REQUIRED_AGE:
-    WEIGHT = int(input("How much do you weigh(kg) "))
-    print("Recommended doseage=", DOSEAGE*WEIGHT,"mg")
-
+if age > 0:
+   if age >= REQUIRED_AGE:
+    print("Recommended two 500mg tablets of paracetamol")
+   elif age < REQUIRED_AGE:
+    weight = int(input("How much do you weigh(kg) "))
+    if weight >0:
+     print("Recommended doseage=", DOSEAGE*weight,"mg")
+     if weight <=0:
+         print("invalid weight")
+else: print("Invalid age")
