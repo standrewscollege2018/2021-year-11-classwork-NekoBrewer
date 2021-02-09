6b1fbb309e4cdbe059caa839893b53b4d1789ce2
NekoBrewer <78179642+NekoBrewer@users.noreply.github.com>
2021-02-09 13:55:28 +1300

    Age in 2021
    Asks for your name and what year you were bron in then it calculates how old you'll be in 2021

---
+++ Age in 2021.py
@@ -0,0 +1,5 @@
+#asks what year you were born and and tells you how old you'll turn in 2021
+year = 2021
+name = input("what is your name? ")
+age = int(input("what year were you born in? "))
+print("If you were born in", age, "then in 2021 you will be", year-age)
