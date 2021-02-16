e5a0ec1b0fa4781baa92681252f86f81766e62d6
NekoBrewer <78179642+NekoBrewer@users.noreply.github.com>
2021-02-16 14:25:20 +1300

    Guessing the number from 1-100
    16.2.2021

--- Guess the random number from 1-10.py
+++ Guess the random number from 1-10.py
@@ -1,19 +1,27 @@
 #makes the number between one and 10 and you must guess it
+high_score = 10000
 import random
-NUM = random.randint(1,10)
+NUM = random.randint(1,100)
 keep_asking = True
 #set the counter
 counter = 0
 while keep_asking == True:
     counter += 1
-    number = int(input("Guess a number between 1 and 10 "))
+    number = int(input("Guess a number between 1 and 100 "))
     if number == NUM:
         keep_asking = False
         print("Good job you guessed the number")
-    if number > NUM and number <=10:
-        print("Go lower")
+    if number > NUM and number <=100:
+        print("Too High Go lower")
     if number < NUM and number >0:
-        print("Go higher")
-    if number > 10 or number <=0:
+        print("Too Low Go higher")
+    if number > 100 or number <=0:
         print("Invalid Number")
-print("You took", counter, "tries")
+if counter < 2:
+    print("you took", counter, "try")
+elif counter >= 2:
+    print("You took", counter, "try's")
+if counter < high_score:
+
+    print("Congratulations you have a new record")
+
