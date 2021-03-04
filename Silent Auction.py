f36ff52142d2a1c769b3d504a4a3dcaedb633a52
NekoBrewer <78179642+NekoBrewer@users.noreply.github.com>
2021-03-05 10:40:33 +1300

    5.3.2021
    Silent Auction

--- Silent Auction.py
+++ Silent Auction.py
@@ -1,17 +1,28 @@
 #This code asks what the auction
+keep_thinking = True
 keep_asking = True
 highest_bid = 0
 Auction_Item = input("What is the item being auctioned? ")
+if Auction_Item >= 0 or < 0:
+    print("Please enter a valid item")
 reserve_price = int(input("What is the reserve price($)? "))
+while keep_thinking == True:
+    if reserve_price < 0:
+        reserve_price = int(input("Please enter a higher reserve price ($)"))
+    if reserve_price > 0:
+        highest_bid = reserve_price
+        keep_thinking = False
 print("The auction for the", Auction_Item, "has started")
+print("The reserve price is $", highest_bid)
+
 while keep_asking == True:
     name = input("What is your name? ")
     bid = int(input("what is your bid?($) "))
-    if bid > highest_bid:
-        bid = highest_bid
     if bid < reserve_price or bid < highest_bid:
         print("Please enter a higher price")
-    print("Highest bid so far is",name,"with",highest_bid)
+    if bid > highest_bid:
+        highest_bid = bid
+        print("Highest bid so far is",name,"with $",highest_bid)
     if bid <= -1:
         keep_asking = False
-print("The auction for the", Auction_Item, "finished at", highest_bid)
+print("The auction for the", Auction_Item, "finished with a bid of $",highest_bid)
