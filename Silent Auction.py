32ca6dae9f425e5a12cfcc3e10c84287ff7d112a
NekoBrewer <78179642+NekoBrewer@users.noreply.github.com>
2021-03-12 09:55:53 +1300

    12.3.2021
    Silent Auction Fixed apart from name being end when you enter end

--- Silent Auction.py
+++ Silent Auction.py
@@ -3,7 +3,9 @@ keep_thinking = True
 keep_asking = True
 highest_bid = 0
 get_item = True
-get
+get_name = True
+bid_checking = True
+reserve_checking = True
 while get_item == True:
     Auction_Item = input("What is the item being auctioned? ")
     if Auction_Item.replace(" ", "") == "":
@@ -20,7 +22,7 @@ while check_reserve == True:
         print("Enter an integer")
 while keep_thinking == True:
     if reserve_price < 0:
-        reserve_checking = True
+
         while reserve_checking == True:
             try:
                 reserve_price = int(input("Please enter a higher reserve price ($) "))
@@ -37,24 +39,32 @@ while keep_asking == True:
 
     while get_name == True:
         name = input("What is your first name? ")
-        if name.replace(" ", "") == "":
-            print("Please enter an name")
-    else:
-        get_item = False
+        if name.replace(" ","") == "":
+            print("Please enter a valid name? ")
+        elif name == "end":
+            keep_asking = False
+            break
+        else:
 
 
-    bid_checking = True
-    while bid_checking == True:
-        try:
-            bid = int(input("what is your bid?($) "))
-            bid_checking = False
-        except:
-            print("Please enter an integer")
-    if bid < reserve_price or bid < highest_bid:
-        print("Please enter a higher price")
-    if bid > highest_bid:
-        highest_bid = bid
-        print("Highest bid so far is",name,"with $",highest_bid)
-    if bid <= -1:
-        keep_asking = False
+
+
+            while bid_checking == True:
+                try:
+                    bid = int(input("what is your bid?($) "))
+                    if bid <= -1:
+                        print("Enter a postive number")
+                    elif bid < reserve_price or bid < highest_bid:
+                        print("Please enter a higher bid")
+                        bid_checking = False
+
+                    else:
+                        highest_bid = bid
+                        print("Highest bid so far is",name,"with $",highest_bid)
+                        bid_checking = False
+                except:
+                    print("Please enter an integer")
+            get_name = True
+            bid_checking = True
+
 print("The auction for the", Auction_Item, "finished with a bid of $",highest_bid, "from", name)
